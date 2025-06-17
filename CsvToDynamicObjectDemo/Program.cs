bb0f0c5ea39f11dfde8816ce618aa65dd293a84d
Nathan Dauny <nathan.dauny@capfi.fr>
2025-06-17 16:48:58 +0200

    first commit

---
+++ CsvToDynamicObjectDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using CSVtoDynamicObjectLib;
+
+namespace CSVtoObject
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "inputs", "example1.csv");
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Fichier CSV non trouvé !");
+                return;
+            }
+            using var fileStream = File.OpenRead(filePath);
+
+            var processor = new CsvProcessor();
+            processor.LoadCsv(fileStream);
+
+            Console.WriteLine("Colonnes et types détectés :");
+            foreach (var colType in processor.ColumnTypes)
+                Console.WriteLine($"{colType.Key} : {colType.Value.Name}");
+
+            Console.WriteLine("\nLignes typées :");
+            foreach (var row in processor.Rows)
+                Console.WriteLine(row);
+        }
+    }
+}
---
+++ CsvToDynamicObjectLib/CsvTypeDetector.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+
+namespace CSVtoObject
+{
+    public static class CsvTypeDetector
+    {
+        private static readonly Type[] CandidateTypes = new Type[]
+        {
+            typeof(int),
+            typeof(double),
+            typeof(bool),
+            typeof(DateTime),
+            typeof(string)
+        };
+
+        public static Type DetectColumnType(IEnumerable<string> values)
+        {
+            foreach (var type in CandidateTypes)
+            {
+                if (type == typeof(string))
+                    continue;
+
+                if (values.All(val => string.IsNullOrWhiteSpace(val) || TryConvert(val, type)))
+                    return type;
+            }
+            return typeof(string);
+        }
+
+        private static bool TryConvert(string val, Type type)
+        {
+            try
+            {
+                if (type == typeof(int))
+                    int.Parse(val, NumberStyles.Integer, CultureInfo.InvariantCulture);
+                else if (type == typeof(double))
+                    double.Parse(val, NumberStyles.Float | NumberStyles.AllowThousands, CultureInfo.InvariantCulture);
+                else if (type == typeof(bool))
+                    bool.Parse(val);
+                else if (type == typeof(DateTime))
+                    DateTime.Parse(val, CultureInfo.InvariantCulture);
+                else
+                    return false;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static object ConvertToType(string val, Type type)
+        {
+            if (string.IsNullOrWhiteSpace(val))
+                return null;
+
+            try
+            {
+                if (type == typeof(int))
+                    return int.Parse(val, NumberStyles.Integer, CultureInfo.InvariantCulture);
+                else if (type == typeof(double))
+                    return double.Parse(val, NumberStyles.Float | NumberStyles.AllowThousands, CultureInfo.InvariantCulture);
+                else if (type == typeof(bool))
+                    return bool.Parse(val);
+                else if (type == typeof(DateTime))
+                    return DateTime.Parse(val, CultureInfo.InvariantCulture);
+                else
+                    return val;
+            }
+            catch
+            {
+                return val;
+            }
+        }
+    }
+}
